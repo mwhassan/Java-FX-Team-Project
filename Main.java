d5bceaa5a0d83443090a2a1bb9985449cfc5f29f
Mostafa <mwhassan@wisc.edu>
2018-04-12 23:26:33 -0500

    Started GUI, made Subunit pane class.
    Style of Subunit needs improvement, GUI not functional.

---
+++ Main.java
@@ -0,0 +1,66 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+
+
+public class Main extends Application {
+	
+	private Stage stage;					// The stage
+	private Scene scene;					// The scene
+	private int width,height;				// Width and height of scene, number of rounds and columns
+	private int rounds,columns;				// Number of rounds and number of columns (for convenience)
+	private HBox primaryLayout;				// Primary bracket layout
+	private VBox[] sublayouts;				// Sub-layouts for each round
+	private Subunit[] subunits;				// Groups submit buttons with scores and team label
+	private Button[] submitButtons;			// Buttons to submit match scores
+	private Label title;					// Title of bracket
+	private Label[] teams;					// Team names
+	private Label[] scores;					// Team scores
+	private Scene display;					// Main scene of the program
+	
+	/**
+	 * Initializes all GUI Objects
+	 * 
+	 * @param width
+	 * @param height
+	 * @param rounds
+	 */
+	private void init (int width, int height, int rounds) {
+		// Parameters from bracket
+		this.width = width;
+		this.height = height;
+		this.rounds = rounds;
+		this.columns = 2*rounds-1;
+		
+		// Primary layout
+		primaryLayout = new HBox(columns);
+		sublayouts = new VBox[columns];
+		subunits = new Subunit[(int)Math.pow(2, rounds)-1];
+//		int counter = rounds;		// tracks which round we are adding
+//		int add = -1;				// add to counter
+//		for(VBox box : sublayouts) {
+//			primaryLayout.getChildren().add(box);
+//		}
+		primaryLayout.getChildren().add(new Subunit(1,"Hi","Hello"));
+		scene = new Scene(primaryLayout,width,height);
+		stage = new Stage();
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO start a bracket from command line argument and replace test values in init()
+		init(300,300,2);
+	}
+}
